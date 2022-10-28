8ed8bb2945194faf9bd4ecd076e1416ddb76935f
diyah eka septyani <diyaheka06@gmail.com>
2022-10-29 05:39:24 +0700

    commit

---
+++ ts_day1.js
@@ -0,0 +1,7 @@
+function ts(text, num1, num2) {
+    return text;
+}
+var text = "Learning Typescript is different than Javascript";
+var num1 = 9;
+var num2 = 19;
+console.log(ts(text, num1, num2));
---
+++ ts_day1.ts
@@ -0,0 +1,8 @@
+function ts(text, num1, num2){
+    return text;
+}
+
+let text: string = "Learning Typescript is different than Javascript";
+let num1: number = 9;
+let num2: number = 19;
+console.log(ts(text,num1, num2));
